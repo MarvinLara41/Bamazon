216bfd0a63998d14116ff66afa86d216ef407a6a
marvinlara41 <laramarvin105@gmail.com>
2021-05-22 11:37:30 -0400

    made data dynamic

---
+++ frontend/src/data.js
@@ -0,0 +1,17 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Blue Dress Shirt",
+      category: "Shirts",
+      image: "/images/BlueDressShirt.jpg",
+      price: 40,
+      brand: "Polo",
+      rating: 4.5,
+      numReviews: 10,
+      description: "High quality dress shirt",
+    },
+  ],
+};
+
+export default data;
