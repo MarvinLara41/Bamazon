c1d79e12dea572a33d1d4b99a7f9f067acdd8111
marvinlara41 <laramarvin105@gmail.com>
2021-05-23 18:39:21 -0400

    added basic backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+import data from "./data.js";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+app.get("/", (req, res) => {
+  res.send("Sever is ready.");
+});
+
+app.listen(port, () => {
+  console.log(`Server at ${port}`);
+});
