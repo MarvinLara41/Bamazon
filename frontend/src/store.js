afe88c00aaf24a92528e25f4fde0f9a250f63fba
marvinlara41 <laramarvin105@gmail.com>
2021-07-02 20:21:54 -0400

    added delete handler

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,7 @@ import thunk from "redux-thunk";
 import { cartReducer } from "./reducer/cartReducers";
 import {
   orderCreateReducer,
+  orderDeleteReducer,
   orderDetailsReducer,
   orderListReducer,
   orderMyListReducer,
@@ -55,6 +56,7 @@ const reducer = combineReducers({
   productUpdate: productUpdateReducer,
   productDelete: productDeleteReducer,
   orderList: orderListReducer,
+  orderDelete: orderDeleteReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
