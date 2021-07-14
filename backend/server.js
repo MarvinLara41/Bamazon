f521456d2d1c9cd1d731f3312b8bfe721ea5136e
marvinlara41 <laramarvin105@gmail.com>
2021-07-13 21:12:48 -0400

    heroku edit

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use((err, req, res, next) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
 )
 } else {
   app.use(express.static("frontend/public"));
